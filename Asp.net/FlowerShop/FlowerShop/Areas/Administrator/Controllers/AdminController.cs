2ece5d503627a84aad5f2d081328818a8522031e
BWolf <haiht.aptech@gmail.com>
2018-11-11 23:36:59 +0700

    the second commit

--- Asp.net/FlowerShop/FlowerShop/Areas/Administrator/Controllers/AdminController.cs
+++ Asp.net/FlowerShop/FlowerShop/Areas/Administrator/Controllers/AdminController.cs
@@ -6,9 +6,9 @@ using System.Web.Mvc;
 
 namespace FlowerShop.Areas.Administrator.Controllers
 {
-    public class HomeController : Controller
+    public class AdminController : Controller
     {
-        // GET: Administrator/Home
+        // GET: Administrator/Admin
         public ActionResult Index()
         {
             return View();
